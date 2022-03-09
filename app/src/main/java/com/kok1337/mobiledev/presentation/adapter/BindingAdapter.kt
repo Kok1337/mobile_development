6c88f41426cc7d12911052df6e465cee276a6e79
kok1337 <aleksey.bondarenko.2001@mail.ru>
2022-03-10 00:47:29 +0700

    Создан BindingAdapter, Добавлена зависимость jdbi

---
+++ app/src/main/java/com/kok1337/mobiledev/presentation/adapter/BindingAdapter.kt
@@ -0,0 +1,49 @@
+package com.kok1337.mobiledev.presentation.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BindingAdapter<T, B : ViewDataBinding>(
+    private val layoutId: Int,
+    private val variableId: Int,
+    onItemClickListener: OnItemClickListener<T>?,
+) : RecyclerView.Adapter<BindingAdapter.BindingViewHolder<B>>() {
+
+    private var items: List<T> = emptyList()
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    private var onItemClickListener: OnItemClickListener<T>? = onItemClickListener
+        set(value) { field = value }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<B> {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<B>(inflater, layoutId, parent, false)
+        return BindingViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BindingViewHolder<B>, position: Int) {
+        val item: T = items[position]
+        val binding: B = holder.binding
+        binding.setVariable(variableId, item)
+        holder.itemView.setOnClickListener { onItemClickListener?.onItemClick(position, item) }
+        viewSetting(binding, item, position)
+    }
+
+    abstract fun viewSetting(binding: B, item: T, position: Int)
+
+    class BindingViewHolder<BH : ViewDataBinding>(val binding: BH) :
+        RecyclerView.ViewHolder(binding.root)
+
+    interface OnItemClickListener<T> { fun onItemClick(position: Int, item: T) }
+
+}
